6cc8ac9a2f793953e02bf1576652eeebe97dac39
beso <beskapoladishvili@gmail.com>
2022-04-28 01:11:23 +0400

    bug fix

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ codes[0].focus()
 codes.forEach((code, idx) => {
   code.addEventListener('keydown', (e) => {
     if (e.key >= 0 && e.key <= 9) {
-      setTimeout(() => {
-        codes[idx + 1].focus()
-      }, 10)
+      codes[idx].value = ''
+      if (idx === codes.length - 1) {
+        codes[idx].focus()
+      } else {
+        setTimeout(() => codes[idx + 1].focus(), 10)
+      }
     }
   })
 })
